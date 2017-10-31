a1eb7caf4bc0a9f61e5d3e64c237000ff89ac0fd
amir bidva <a.bidva@gmail.com>
2017-10-31 15:33:32 -0400

    adding routes content

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,8 +8,8 @@ const Header = () => (
     <nav>
       <ul>
         <li><Link to='/'>Home</Link></li>
-        <li><Link to='/roster'>Roster</Link></li>
-        <li><Link to='/schedule'>Schedule</Link></li>
+      <li><Link to='/about'>About</Link></li>
+    <li><Link to='/contact'>Contact</Link></li>
       </ul>
     </nav>
   </header>
