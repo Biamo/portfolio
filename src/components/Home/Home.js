2a17c2961de595d49dc8ba77365daa549fb388bd
amir bidva <a.bidva@gmail.com>
2017-11-02 11:03:35 -0400

    add text to home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,38 @@
 import React from 'react'
 import './Home.css'
+import Typist from 'react-typist';
+
 const Home = () => (
-  <div class="fullscreen-bg">
-    <div class="video-background">
+<div class="fullscreen-bg">
+  <div class="video-background">
     <div class="video-foreground">
-      <iframe title="back-video" src="https://www.youtube.com/embed/p-i__OxJp40?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=W0LHTWG-UmQ" frameborder="0" allowfullscreen></iframe>
+      <iframe title="back-video" src="https://www.youtube.com/embed/videoseries?list=PLtQt647Xg_xFC8AfXw2TYdgpILqjOFqHy&&autoplay=1&loop=1&amp;controls=0&amp;showinfo=0?ecver=2" frameborder="0" allowfullscreen></iframe>
     </div>
   </div>
-
-<div id="vidtop-content">
-<div class="vid-info">
-	  <h1>YouTube Fullscreen Background Demo</h1>
-	  <p>The International Space Station orbits the Earth every 92 minutes, with its crew seeing a sunrise 15 times a day. It exists as a scientific, educational, and engineering platform in low orbit, 330 to 435 kilometres above the Earth.</p>
-     <p>Original timelapse by Riccardo Rossi (ISAA), used under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. Raw photos courtesy of http://eol.jsc.nasa.gov/</p>
-	 <a href="/500/Use-YouTube-Videos-as-Fullscreen-Web-Page-Backgrounds">Full article</a>
+  <div id="vidtop-content">
+    <div class="vid-info">
+      <Typist>
+        <p> Hi, My name is Amir. </p>
+        <Typist.Delay ms={500} />
+        <br />
+        <p> I'm a fullstack developer </p>
+        <Typist.Delay ms={500} />
+        <br />
+        <ul>
+          <li>6+ years of experience in software design and development in different environments as a Full-Stack Developer.</li>
+          <li>6+ years as C# programmer, expert at coding, debugging, refactoring and optimizing.</li>
+          <li>3 years as Ruby programmer, expert at coding, debugging, refactoring and optimizing.</li>
+          <li>3 years as Modern Javascript developer, expert at coding, debugging, refactoring and optimizing.</li>
+          <li>Good at high-performance, high-availability, high-scalability system architecture design.</li>
+          <li>Experienced in development management, familiar with methodologies and relative tools.</li>
+          <li>Experienced with Big data processing and familiar with relative tools.</li>
+          <li>Experienced with distributed structures, unit testing (TDD), auto deployment.</li>
+          <li>Responsible person with good communication and collaboration capability.</li>
+        </ul>
+      </Typist>
+    </div>
   </div>
 </div>
-  </div>
 )
 
 export default Home
