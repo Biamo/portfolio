8ffdbec8f820a1889b9859937a32c4a2918fadaa
amir bidva <a.bidva@gmail.com>
2017-11-02 18:31:39 -0400

    adding logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import {Navbar, Nav, NavItem} from 'react-bootstrap';
+import logo from './logo.gif';
+import './Header.css'
 
 export default class Header extends React.Component {
 
 render() {
     return (
-      <Navbar>
+      <Navbar inverse>
        <Navbar.Header>
          <Navbar.Brand>
-           <a href="/">React-Bootstrap</a>
+           <a href="/"><img className="header-logo" src={logo}/></a>
          </Navbar.Brand>
        </Navbar.Header>
        <Nav>
-         <NavItem eventKey={1} href="/">Home</NavItem>
-       <NavItem eventKey={2} href="/about">About</NavItem>
-     <NavItem eventKey={3} href="/contact">contact</NavItem>
+          <NavItem eventKey={1} href="/">Home</NavItem>
+          <NavItem eventKey={2} href="/about">About</NavItem>
+     <NavItem eventKey={3} href="/contact">Contact</NavItem>
        </Nav>
      </Navbar>
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,34 +1,42 @@
 import React from 'react'
 import './Home.css'
 import Typist from 'react-typist';
+// import Iframe from '../Shared/Iframe/Iframe'
 
 const Home = () => (
 <div class="fullscreen-bg">
   <div class="video-background">
     <div class="video-foreground">
-      <iframe title="back-video" src="https://www.youtube.com/embed/videoseries?list=PLtQt647Xg_xFC8AfXw2TYdgpILqjOFqHy&&autoplay=1&loop=1&amp;controls=0&amp;showinfo=0?ecver=2" frameborder="0" allowfullscreen></iframe>
+      <iframe title="back-video" src="https://www.youtube.com/embed/videoseries?list=PLtQt647Xg_xFC8AfXw2TYdgpILqjOFqHy&autoplay=1&loop=1&controls=0&showinfo=0?ecver=2" frameborder="0" allowfullscreen></iframe>
     </div>
   </div>
   <div id="vidtop-content">
     <div class="vid-info">
       <Typist>
-        <p> Hi, My name is Amir. </p>
+        <h1> Hi, My name is <b>Amir</b>. </h1>
         <Typist.Delay ms={500} />
         <br />
-        <p> I'm a fullstack developer </p>
+        <p> {"I'm a fullstack developer"} </p>
         <Typist.Delay ms={500} />
         <br />
-        <ul>
-          <li>6+ years of experience in software design and development in different environments as a Full-Stack Developer.</li>
-          <li>6+ years as C# programmer, expert at coding, debugging, refactoring and optimizing.</li>
-          <li>3 years as Ruby programmer, expert at coding, debugging, refactoring and optimizing.</li>
-          <li>3 years as Modern Javascript developer, expert at coding, debugging, refactoring and optimizing.</li>
-          <li>Good at high-performance, high-availability, high-scalability system architecture design.</li>
-          <li>Experienced in development management, familiar with methodologies and relative tools.</li>
-          <li>Experienced with Big data processing and familiar with relative tools.</li>
-          <li>Experienced with distributed structures, unit testing (TDD), auto deployment.</li>
-          <li>Responsible person with good communication and collaboration capability.</li>
-        </ul>
+          <span>{'6+ years of experience in software design and development in different environments as a Full-Stack Developer.'}</span>
+          <Typist.Backspace count={110} delay={200} />
+          <span>{'6+ years as C# programmer, expert at coding, debugging, refactoring and optimizing.'}</span>
+          <Typist.Backspace count={83} delay={200} />
+          <span>{'3 years as Ruby programmer, expert at coding, debugging, refactoring and optimizing.'}</span>
+          <Typist.Backspace count={84} delay={200} />
+          <span>{'3 years as Modern Javascript developer, expert at coding, debugging, refactoring and optimizing.'}</span>
+          <Typist.Backspace count={96} delay={200} />
+          <span>{'Good at high-performance, high-availability, high-scalability system architecture design.'}</span>
+          <Typist.Backspace count={89} delay={200} />
+          <span>{'Experienced in development management, familiar with methodologies and relative tools.'}</span>
+          <Typist.Backspace count={86} delay={200} />
+          <span>{'Experienced with Big data processing and familiar with relative tools.'}</span>
+          <Typist.Backspace count={70} delay={200} />
+          <span>{'Experienced with distributed structures, unit testing (TDD), auto deployment.'}</span>
+          <Typist.Backspace count={77} delay={200} />
+          <span>{'Responsible person with good communication and collaboration capability.'}</span>
+          <Typist.Backspace count={72} delay={200} />
       </Typist>
     </div>
   </div>
